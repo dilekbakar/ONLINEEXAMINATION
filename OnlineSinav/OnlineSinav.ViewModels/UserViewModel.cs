2130affa179da36891530bfa6368f88dd81bac67
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2022-08-16 10:21:20 +0300

    commit
    new commit from other PC

--- OnlineSinav/OnlineSinav.ViewModels/UserViewModel.cs
+++ OnlineSinav/OnlineSinav.ViewModels/UserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineSinav.Data.Model;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace OnlineSinav.ViewModels
@@ -6,10 +7,26 @@ namespace OnlineSinav.ViewModels
     public class UserViewModel
     {
 
-        public UserViewModel()
+        public UserViewModel(Users model)
         {
-
+            ID = model.ID;
+            Name = model.Name ?? "";
+            UserName = model.UserName;
+            Password = model.Password;
+            Role = model.Role;
         }
+        public Users ContentViewModel(UserViewModel vm)
+        {
+            return new Users
+            {
+                ID = vm.ID,
+                Name = vm.Name,
+                UserName = vm.UserName,
+                Password = vm.Password,
+                Role = vm.Role,
+            };
+        }
+
         public int ID { get; set; }
 
         [Required]
